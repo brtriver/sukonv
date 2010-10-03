2584aec10f4eafac26f90eeca22db0933fd3fe64
hidenorigoto <hidenorigoto@gmail.com>
2010-10-03 11:05:18 +0900

    added: tags

--- app/views/questions/index.html.php
+++ app/views/questions/index.html.php
@@ -4,8 +4,8 @@
     <h3><?=$question->title ?></h3>
 	[<?=$this->html->link('edit', 'questions/edit/' . $question->_id) ?>]
     <p><?=$question->description ?></p>
-    <p><?=$this->html->link('view detail', 'questions/view/' . $question->_id) ?></p>
     <p class='tag-list'>tag:<span class='tag'><?php echo implode("</span>,<span class='tag'>", $question->tag) ?></span></p>
+    <p><?=$this->html->link('view detail', 'questions/view/' . $question->_id) ?></p>
 </article>
 <?php endforeach; ?>
 
